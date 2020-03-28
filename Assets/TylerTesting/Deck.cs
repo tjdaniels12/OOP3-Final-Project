fdf231697490f5b57faa49a72981c6ef28cc3588
Tyler Daniels <47905367+tjdaniels12@users.noreply.github.com>
2020-03-28 19:16:46 -0400

    hi

---
+++ Assets/TylerTesting/Deck.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Deck : MonoBehaviour
+{
+    public const float maxWidth = 100f;
+    public const float minWidth = 0f;
+    public float width = 100f;
+
+
+
+    public GameObject Card;
+    public Image DeckPic;
+    public HorizontalLayoutGroup Hand;
+
+    List<Card> deck = new List<Card>();
+    Card card;
+
+   
+    void Start()
+    {
+        //card = Resources.Load<Card>("dug");
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        Debug.Log(deck.Count);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            print("space key was pressed");
+
+            DrawCard();
+            
+
+        }
+    }
+
+    void DrawCard()
+    {
+        if (deck.Count != 0)
+        {
+            var go = Instantiate(Card, new Vector3(0, 0), Quaternion.identity);
+            go.transform.SetParent(Hand.transform);
+            CardDisplay display = go.GetComponent<CardDisplay>();
+            display.card = deck[0];
+            deck.RemoveAt(0);
+            Debug.Log(deck.Count);
+            width = (float)deck.Count * 2.5f;
+            Debug.Log(width);
+            if (width > 100f)
+            {
+                width = 100f;
+                Debug.Log("Capped to 40.");
+            }
+            DeckPic.rectTransform.sizeDelta = new Vector2(width, 100);
+            Debug.Log(width);
+        }
+        //else fatigue();
+    }
+
+}
---
+++ Assets/TylerTesting/DrawButton.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawButton : MonoBehaviour
+{
+    public Button drawButton;
+    public const float maxWidth = 100f;
+    public const float minWidth = 0f;
+    public float width = 100f;
+    public GameObject Card;
+    public Image Deck;
+    public HorizontalLayoutGroup Hand;
+
+    List<Card> deck = new List<Card>();
+    Card card;
+
+
+    void Start()
+    {
+        //card = Resources.Load<Card>("dug");
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        deck.Add(Resources.Load<Card>("dug"));
+        deck.Add(Resources.Load<Card>("doggo"));
+        Debug.Log(deck.Count);
+        Button btn = drawButton.GetComponent<Button>();
+        btn.onClick.AddListener(DrawCard);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void DrawCard()
+    {
+        if (deck.Count != 0)
+        {
+            var go = Instantiate(Card, new Vector3(0, 0), Quaternion.identity);
+            go.transform.SetParent(Hand.transform);
+            CardDisplay display = go.GetComponent<CardDisplay>();
+            display.card = deck[0];
+            deck.RemoveAt(0);
+            Debug.Log(deck.Count);
+            width = (float)deck.Count * 2.5f;
+            Debug.Log(width);
+            if (width > 100f)
+            {
+                width = 100f;
+                Debug.Log("Capped to 40.");
+            }
+            Deck.rectTransform.sizeDelta = new Vector2(width, 100);
+            Debug.Log(width);
+        }
+    }
+
+}
